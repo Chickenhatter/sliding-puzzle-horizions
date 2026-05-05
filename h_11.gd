f07c533178d9c81b9441f45058c17401581311dd
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-05 22:14:29 +1200

    First 30m commit, for some reason moving the puzzle piece was hard

---
+++ h_11.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+var v = 0
+var dragging = false
+var inside = false
+var mouse_pos = false
+var distance = false
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var direction: Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO
+	
+	mouse_pos = get_global_mouse_position()
+	direction = global_position.direction_to(mouse_pos)
+
+	if inside == true:
+		if Input.is_action_just_pressed("ui_y"):
+			if dragging == true:
+				dragging = false
+			elif dragging == false:
+				dragging = true
+
+	if dragging == true:
+		print("a")
+		velocity = direction * 300
+
+	move_and_slide()
+
+func _on_h_11_mouse_entered() -> void:
+	inside = true
+	print('a')
+
+
+func _on_h_11_mouse_exited() -> void:
+	inside = false
+	print('e')
